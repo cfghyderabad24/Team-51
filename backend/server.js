5a14cc1ff4c1b1d1087e86a61b821e0eabbac11a
Anushka Mamidi <anushkaa0418@gmail.com>
2024-06-22 19:20:14 +0530

    model&routes

--- backend/server.js
+++ backend/server.js
@@ -1,44 +1,31 @@
-const exp=require('express')
-const app=exp()
-require('dotenv').config()
+require('dotenv').config();
+const express = require("express");
 const mongoose = require("mongoose");
-const path=require('path')
-const cors=require('cors')
 
-app.use(cors({
-    origin: 'http://localhost:3000', // Adjust according to your frontend URL
-    methods: 'GET,POST,PUT,DELETE',
-    allowedHeaders: 'Content-Type,Authorization'
-}));
 
-//app.use(exp.static(path.join(__dirname,'../client/build')))
+//express app
+const app = express();
+const productRoutes = require('./routes/productRoutes.js');
 
+
+//middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.method, req.path)
+    next();
+})
+
+
+
+//connect to db
 mongoose.connect(process.env.DB_URL)
     .then(() => {
         //listen to requests
-        
-            console.log("connected to db and Listening");
+        app.listen(process.env.PORT, () => {
+            console.log("connected to db and Listening", process.env.PORT);
+        })
     })
     .catch((error) => {
         console.log(error)
     })
-
-
-app.use(exp.json())
-
-
-
-
-
-
-/*app.use((req,res,next)=>{
-    res.sendFile(path.join(__dirname,'../client/build/index.html'))
-})*/
-
-app.use((err,req,res,next)=>{
-    res.send({messgae:'error',payload:err.message})
-})
-
-
-const port=process.env.PORT || 5000;
-app.listen(port,()=>{console.log(`Web server on port ${port}`)})
